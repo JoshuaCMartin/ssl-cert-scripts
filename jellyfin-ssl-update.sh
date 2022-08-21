5f9ed27c5ae12a69db5179f86af0662e9818bffc
Joshua Martin <51254272+JoshuaCMartin@users.noreply.github.com>
2022-08-21 07:30:53 -0700

    Create jellyfin-ssl-update.sh

---
+++ jellyfin-ssl-update.sh
@@ -0,0 +1,25 @@
+  GNU nano 5.4                                                                                          update_ssl.sh                                                                                                    
+#!/bin/bash
+
+# Call this script with the paths to your cert.pem(1) and key.pem(2) files from LetsEncrypt and finally the output cert.pfx(3)
+# The generated SSL certificate can be sent anywhere, as long as the user jellyfin has permissions to read that directory. I use /var/lib/jellyfin.
+# If using OPNSense ACME Plugin to upload, they will by default be in ~/[site-name].com/[cert-type].pem
+# After generating a pfx cert file, you must select the file as your cert in the Jellyfin Web Interface, under Dashboard > Networking > Custom SSL certificate path:
+# Selecting the cert in the web GUI only need be done once, but the service must be restarted every time the cert is updated.
+# "passout pass:" leaves the password for the exported key empty. 
+# Simply change to "-passout pass: [Your Password]" and enter it in the Jellyfin Web Interface if you'd like
+
+
+# Example:
+# ./jellyfin-update-ssl.sh /root/example.com/cert.pem /root/example.com/key.pem /var/lib/jellyfin/cert.pfx
+
+
+# SCRIPT BEGIN  ----------
+cert="${1}"
+key="${2}"
+jellycert="${3}"
+
+openssl pkcs12 -export -in "${cert}" -inkey "${key}" -out "${jellycert}" -passout pass:
+chown jellyfin:jellyfin "${jellycert}"
+systemctl restart jellyfin
+# SCRIPT END    ----------
