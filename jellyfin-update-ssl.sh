86012c52d80675e66086922ea5f6ff3c276228ea
Joshua Martin <51254272+JoshuaCMartin@users.noreply.github.com>
2022-08-21 07:35:46 -0700

    Update jellyfin-update-ssl.sh

--- jellyfin-update-ssl.sh
+++ jellyfin-update-ssl.sh
@@ -2,7 +2,7 @@
 
 # Call this script with the paths to your cert.pem(1) and key.pem(2) files from LetsEncrypt and finally the output cert.pfx(3)
 # The generated SSL certificate can be sent anywhere, as long as the user jellyfin has permissions to read that directory. I use /var/lib/jellyfin.
-# If using OPNSense ACME Plugin to upload, they will by default be in ~/[site-name].com/[cert-type].pem
+# If using OPNSense ACME Plugin to upload, they will by default be in [User Home Dir]/[site-name].com/[cert-type].pem
 # After generating a pfx cert file, you must select the file as your cert in the Jellyfin Web Interface, under Dashboard > Networking > Custom SSL certificate path:
 # Selecting the cert in the web GUI only need be done once, but the service must be restarted every time the cert is updated.
 # passout pass: leaves the password for the exported key empty. 
