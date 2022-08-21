123c129db394dffc6dc8dea42de423fb2742a52f
Joshua Martin <51254272+JoshuaCMartin@users.noreply.github.com>
2022-08-21 09:41:04 -0700

    Update proxmox-update-ssl.sh

--- proxmox-update-ssl.sh
+++ proxmox-update-ssl.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+# EXAMPLE
 # ./proxmox-update-ssl.sh /path/to/fullchain.pem /path/to/key.pem node-name
 
+# SCRIPT BEGIN  ----------
 if [[ $(id -u) -ne 0 ]]; then
   echo "This script must be executed as root or using sudo."
   exit 99
@@ -9,3 +13,4 @@ chown www-data:www-data "${2}" &&
 cp "${1}" /etc/pve/nodes/"${3}"/pveproxy-ssl.pem &&
 cp "${2}" /etc/pve/nodes/"${3}"/pveproxy-ssl.key &&
 systemctl restart pveproxy
+# SCRIPT END    ----------
